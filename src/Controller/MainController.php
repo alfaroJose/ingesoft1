fcfc9bee7a26a146a063734b395585c30815fca0
Andres Solis Aponte <b26505@ecci.ucr.ac.cr>
2018-09-07 15:14:12 -0600

    Agregado index

---
+++ src/Controller/MainController.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Controller;
+
+use App\Controller\AppController;
+
+/**
+ * Main Controller
+ *
+ */
+class MainController extends AppController
+{
+	public function index()
+    {
+        
+    }
+}
